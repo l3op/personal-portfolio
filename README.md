2bf1b2ccefaa7d6f7ab35614ebf3099a483e6406
Lucas <c8li@hotmail.com>
2023-09-19 03:40:06 +0300

    init project

---
+++ README.md
@@ -0,0 +1,4 @@
+This is my personal portofolio !
+
+> [!IMPORTANT]
+> The repository is still under developing :hammer_and_wrench:
---
+++ next.config.js
@@ -0,0 +1,11 @@
+// @ts-check
+
+/**
+ * @type {import('next').NextConfig}
+ **/
+const nextConfig = {
+  output: 'export',
+  basePath: '/personal-portfolio',
+}
+
+module.exports = nextConfig
